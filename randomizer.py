14139714f79d30cc64467d38c6badf8463a2fe4d
Tyler K <tylerkruer1@gmail.com>
2023-12-03 16:10:32 -0700

    day 3 in Rust

--- randomizer.py
+++ randomizer.py
@@ -1,12 +1,19 @@
 import random
 import datetime
+import json
 
-languages = [
-    'Python', 'Java', 'C++', 'C#', 'JavaScript', 'Ruby', 
-    'PHP', 'C', 'Swift', 'Go', 'Objective-C', 'Mojo', 'Cuda',
-    'Kotlin', 'Scala', 'Rust', 'Groovy', 'Dart', 'Haskell', 
-    'Lua', 'Julia', 'Elixir', 'Clojure',  'Erlang', 'OCaml', 
-    'TypeScript', 
-]
+with open("languages.json", "r") as f:
+    data = json.load(f)
 
-print(f"Today for {datetime.datetime.now().today().strftime('%m/%d')} you're doing AOC in {random.choice(languages)}. Good luck!")
+# Choose a random language and remove it from possible_languages
+selected_language = random.choice(data['possible_languages'])
+data['possible_languages'].remove(selected_language)
+
+# Add the selected language to completed_languages
+data['completed_languages'].append(selected_language)
+
+# Update the JSON file
+with open("languages.json", "w") as f:
+    json.dump(data, f, indent=4)
+
+print(f"Today for {datetime.datetime.now().today().strftime('%m/%d')} you're doing AOC in {selected_language}. Good luck!")
